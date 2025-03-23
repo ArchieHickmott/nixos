567129ee203bed07729541a949d13acfa9b9db56
Archie <25hickmar@gmail.com>
2025-03-24 06:54:43 +1000

    Commit 16

--- modules/global-config.nix
+++ modules/global-config.nix
@@ -2,5 +2,9 @@
 {
     config = {
         nix.settings.experimental-features = [ "nix-command" "flakes" ];
+        environment.systemPackages = with pkgs; [
+            nixd
+        ];
+        nix.nixPath = [ "nixpkgs=${pkgs.path}" ]; 
     };
 }
