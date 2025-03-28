fe981317409eb53b184b254a4dc8a5e92633388d
Archie <25hickmar@gmail.com>
2025-03-28 11:52:32 +1000

    Commit 19

---
+++ modules/doom/default.nix
@@ -0,0 +1,49 @@
+{ config, pkgs, lib, ... }:
+
+{
+  options.doom.enable = lib.mkEnableOption "Doom Emacs configuration";
+
+  config = lib.mkIf config.doom.enable {
+    home.file = {
+      ".doom.d".source = ../users/archie/dotfiles/.doom.d;
+    };
+
+    home.packages = with pkgs; [
+      emacs                         # Emacs itself
+      git                           # Required for updates
+      ripgrep                       # Search functionality
+      fd                            # Fast file searching
+      gnutls                        # TLS support
+      zstd                          # Compression
+      coreutils                     # Basic utilities
+      findutils                     # Find command
+      gnugrep                       # Grep support
+      gawk                          # Required for Doom scripts
+      curl                          # Fetching external resources
+      unzip                         # Extracting archives
+      xclip                         # Clipboard support (X11)
+      wl-clipboard                  # Clipboard support (Wayland)
+      fzf                           # Fuzzy finding
+      tree-sitter                   # Syntax highlighting
+      sqlite                        # Needed for org-roam
+      nodejs                        # Language Server Protocol (LSP)
+      yarn                          # Required for some LSP servers
+      python3                       # Python support
+      nixfmt                        # Nix formatting
+
+      # Fonts for Emacs
+      nerdfonts                     # Icons
+      fontconfig                    # Font detection
+    ];
+
+    # Ensure Doom Emacs is installed
+    home.activation = {
+      doom-emacs = ''
+        if [ ! -d "$HOME/.emacs.d" ]; then
+          git clone --depth 1 https://github.com/doomemacs/doomemacs ~/.emacs.d
+          ~/.emacs.d/bin/doom install
+        fi
+      '';
+    };
+  };
+}
--- users/archie/home.nix
+++ users/archie/home.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 {
   # Make sure this matches your system user:
@@ -6,34 +6,11 @@
   home.homeDirectory = "/home/archie";
   nixpkgs.config.allowUnfree = true;
 
-  # Example: console configuration
-  # If you want custom fonts or other console-related tweaks:
-  # programs.somethingConsole = { ... };
+  imports = [ ../../modules/doom.nix ];
 
-  # Example: plain Emacs
-  programs.emacs = {
-    enable = true;
-    package = pkgs.emacs;
-  };
+  doom.enable = True;
 
-  # Example: installing doom-emacs plus your own config
-  # There are several ways to handle Doom. One approach:
-  #  1. Let Home Manager install doom-emacs.
-  #  2. Source your doom config from your dotfiles or a separate flake.
-  #
-  # If you keep your doom config in ~/.doom.d, you can do:
-  # home.file.".doom.d/init.el".source = ./doom/init.el;
-  # home.file.".doom.d/config.el".source = ./doom/config.el;
-  # home.file.".doom.d/packages.el".source = ./doom/packages.el;
-  #
-  # Or store them in a separate repo. For example:
-  # home.file.".doom.d/init.el".source = "${yourDoomRepo}/init.el";
-  #
-  # Then override 'programs.emacs.package' to point to doom if you like.
-
-  # Example: packages you want installed at the user level
   home.packages = with pkgs; [
-    # Some random goodies
     htop
     wget
     kdePackages.kate
@@ -41,9 +18,7 @@
     rpi-imager
     steam
     maturin
-    musescore
   ];
 
-  # You can do the same pattern for other apps:
-  # xdg.configFile."foo/bar.conf".source = ./bar.conf;
+  programs.home-manager.enable = true;
 }
